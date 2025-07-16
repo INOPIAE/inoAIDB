3c6a216975e6717559c2990cf1adc850274f4d13
INOPIAE <m.maengel@inopiae.de>
2025-07-16 07:20:20 +0200

    upd: make NavBar responsive

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,53 +1,78 @@
 <template>
-  <v-navigation-drawer>
-    <template v-for="(item, index) in items" :key="index">
-      <v-list-item v-if="!item.condition || item.condition()">
-        <template v-if="item.style === 'divider'">
-          <v-divider />
-        </template>
-        <template v-else-if="item.style === 'external'">
-          <a
-            :href="item.href"
-            :style="{ paddingLeft: `${item.level ? item.level * 20 : 0}px` }"
-            target="_blank"
-            rel="noopener noreferrer"
-            @click="item.onClick ? item.onClick() : null"
-          >
-            <v-icon :icon="item.icon" />
-            <span class="link-text">{{ t(item.title) }}</span>
-          </a>
-        </template>
-        <template v-else>
-          <router-link
-            :to="item.href"
-            :style="{ paddingLeft: `${item.level ? item.level * 20 : 0}px` }"
-            @click="item.onClick ? item.onClick() : null"
-          >
-            <v-icon :icon="item.icon" />
-            <span class="link-text">{{ t(item.title) }}</span>
-          </router-link>
-        </template>
-      </v-list-item>
-    </template>
+  <v-navigation-drawer
+    v-model="drawerModel"
+    app
+    :temporary="isMobile"
+    :permanent="!isMobile"
+  >
+    <v-list density="compact">
+      <template v-for="(item, index) in items" :key="index">
+        <v-list-item v-if="!item.condition || item.condition()">
+          <template v-if="item.style === 'divider'">
+            <v-divider />
+          </template>
+          <template v-else-if="item.style === 'external'">
+            <a
+              :href="item.href"
+              :style="{ paddingLeft: `${item.level ? item.level * 20 : 0}px` }"
+              target="_blank"
+              rel="noopener noreferrer"
+              @click="item.onClick ? item.onClick() : null"
+            >
+              <v-icon :icon="item.icon" />
+              <span class="link-text">{{ t(item.title) }}</span>
+            </a>
+          </template>
+          <template v-else>
+            <router-link
+              :to="item.href"
+              :style="{ paddingLeft: `${item.level ? item.level * 20 : 0}px` }"
+              @click="() => handleItemClick(item)"
+            >
+              <v-icon :icon="item.icon" />
+              <span class="link-text">{{ t(item.title) }}</span>
+            </router-link>
+          </template>
+        </v-list-item>
+      </template>
+    </v-list>
   </v-navigation-drawer>
 </template>
 
 <script>
 import { computed } from "vue";
 import { useAuthStore } from "@/stores/auth";
-import { useI18n } from 'vue-i18n'
+import { useI18n } from "vue-i18n";
+import { useDisplay } from "vuetify";
 
 export default {
-    setup() {
-        const authStore = useAuthStore();
-        const { t } = useI18n()
-        const backendUrl = import.meta.env.VITE_BACKEND_URL || 'http://localhost:8000'
-        const raw_mount = import.meta.env.VITE_ROOT_MOUNT;
-        const root_mount = (!raw_mount || raw_mount.toLowerCase() === "none") ? "" : raw_mount;
+  props: {
+    drawer: Boolean
+  },
+  emits: ['update:drawer'],
+  setup(props, { emit }) {
+    const { mobile } = useDisplay();
+    const isMobile = computed(() => mobile.value);
 
-        console.log(backendUrl)
-        console.log(root_mount)
-        console.log(`${backendUrl}${root_mount}/docs`)
+    const updateDrawer = (val) => emit('update:drawer', val);
+
+    const drawerModel = computed({
+      get: () => props.drawer,
+      set: (val) => updateDrawer(val)
+    });
+
+    const authStore = useAuthStore();
+    const { t } = useI18n();
+
+    const backendUrl = import.meta.env.VITE_BACKEND_URL || "http://localhost:8000";
+    const raw_mount = import.meta.env.VITE_ROOT_MOUNT;
+    const root_mount = (!raw_mount || raw_mount.toLowerCase() === "none") ? "" : raw_mount;
+
+    const handleItemClick = (item) => {
+      if (item.onClick) item.onClick();
+      if (isMobile.value) updateDrawer(false);
+    };
+
         const items = computed(() => [
             {
                 title: 'home',
@@ -169,30 +194,33 @@ export default {
             },
         ]);
 
-        return {
-            authStore,
-            items,
-            t
-        };
-    },
+
+    return {
+      drawerModel,
+      isMobile,
+      items,
+      handleItemClick,
+      t
+    };
+  }
 };
 </script>
 
 <style scoped>
 a {
-    text-decoration: none;
-    font-weight: normal;
+  text-decoration: none;
+  font-weight: normal;
 }
 
 a .link-text {
-    padding-left: 4px;
+  padding-left: 4px;
 }
 
 a:hover .link-text, a:active .link-text {
-    text-decoration: underline;
+  text-decoration: underline;
 }
 
 .router-link-active {
-    font-weight: bold;
+  font-weight: bold;
 }
 </style>
